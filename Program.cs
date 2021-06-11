7c91785213568030496441b85889c4c95267fa64
amelia <amelia.izak@gmail.com>
2021-06-11 13:54:24 +0200

    files added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace altkomAmeliaStosik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today is Friday!");
+        }
+    }
+}
